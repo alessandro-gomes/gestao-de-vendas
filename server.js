cbc7f611fc5204242c1855523307b8c7dc64775d
alessandro-gomes <alessandro_gomes@outlook.com.br>
2024-06-20 22:24:21 -0300

    criacao da pagina para cadastrar cliente, criacao do webserver e adicao de modulos do node.

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var url = require('url');
+
+app.use(express.static('public'));
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+const list = { "list": [ 
+    { id: 1, nome: "Alessandro" }
+]};
+
+app.get('/task', function (req, res) {
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   const resp = JSON.stringify(list);
+   res.end(resp);
+});
+
+app.post('/register', function (req, res) {
+   console.log(req.body);
+   list.list.push(req.body);
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   res.end('{ "msg": "Sucesso" }');
+});
+
+var server = app.listen(3000, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("Example app listening at http://%s:%s", host, port);
+});
+
